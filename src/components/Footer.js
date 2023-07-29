528b2d24b4df366c60e429e8f248aa5f35d16958
Akash Mandal <akashmandal6297@gmail.com>
2023-07-29 14:14:16 +0530

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,16 +10,15 @@ const Footer = () => {
       <div className="footer-content">
       <section>
         <div className="footer-links">
-          <a href="/">Home</a>
-          <br/>
-          <a href="/about">About</a>
-          <br/>
-          <a href="/process">Process</a>
-          <br/>
+          <a href="/"><i className="fas fa-home"></i></a>
+          
+          <a href="/about"><i className="fas fa-info-circle"></i></a>
+          
+          <a href="/process"><i className="fas fa-cogs"></i></a>
+          
           
         </div>
-        <br/>
-        <br/>
+        
        
         </section>
         <section>
@@ -30,21 +29,21 @@ const Footer = () => {
         <br/>
         
         
-        </section>
-        <section>
+        {/* </section>
+        <section> */}
         <div className="footer-social">
           <a href="https://www.facebook.com/yourfacebookpage" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faFacebookF} />FACEBOOK
+            <FontAwesomeIcon icon={faFacebookF} />
           </a>
-        </div>
-        <div>
+        {/* </div>
+        <div> */}
           <a href="https://twitter.com/yourtwitterpage" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} />TWITTER
+            <FontAwesomeIcon icon={faTwitter} />
           </a>
-          </div>
-          <div>
+          {/* </div>
+          <div> */}
           <a href="https://www.instagram.com/yourinstagrampage" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faInstagram} />INSTAGRAM
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
           
           {/* Add more social media icons and links as needed */}
