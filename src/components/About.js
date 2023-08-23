7e19be65672cfc4253ee3e7da13822de36840e2f
Akash Mandal <akashmandal6297@gmail.com>
2023-08-23 23:04:02 +0530

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import '../css/about.css'; // Import the CSS file for the about page styles
 import Image1 from '../images/Akash.png';
-import Image2 from '../images/Shweta.png';
-import Image3 from '../images/Rupak.png';
-import Image4 from '../images/Gobinda.jpg';
+import Image2 from '../images/webp/Shweta.webp';
+import Image3 from '../images/webp/Rupak.webp';
+import Image4 from '../images/webp/Gobinda.webp';
 import Image5 from '../images/Akash.png';
-import Image6 from '../images/Joydeep.jpg';
-import Image7 from '../images/Souvik.jpg';
-import Image8 from '../images/Shourdha.jpg';
+import Image6 from '../images/webp/Joydeep.webp';
+import Image7 from '../images/webp/Souvik.webp';
+import Image8 from '../images/webp/Shourdha.webp';
 import Image9 from '../images/Akash.png';
 import Image10 from '../images/Akash.png';
 const About = () => {
