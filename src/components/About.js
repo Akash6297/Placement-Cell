812e4e62d8a0eb69c818e16be460bd98344c6f84
Akash Mandal <akashmandal6297@gmail.com>
2023-08-23 22:15:55 +0530

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -1,65 +1,12 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import '../css/about.css'; // Import the CSS file for the about page styles
-
-// const About = () => {
-//   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-//   const [submitMessage, setSubmitMessage] = useState('');
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post('https://placement-p2k8.onrender.com/api/contact', formData);
-//       console.log(response.data);
-//       setSubmitMessage('Your message has been submitted. We will get back to you soon.');
-//     } catch (error) {
-//       console.error('Error submitting the contact form:', error);
-//       setSubmitMessage('Oops! Something went wrong. Please try again later.');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div className="about-section">
-//         <h2>About Page</h2>
-//         <p>This is the About page content.</p>
-//       </div>
-//       <div className="contact-section">
-//         <h3>Contact Us</h3>
-//         <form className="contact-form" onSubmit={handleSubmit}>
-//           <div>
-//             <label htmlFor="name">Name:</label>
-//             <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-//           </div>
-//           <div>
-//             <label htmlFor="email">Email:</label>
-//             <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-//           </div>
-//           <div>
-//             <label htmlFor="message">Message:</label>
-//             <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
-//           </div>
-//           <button type="submit">Submit</button>
-//         </form>
-//         {submitMessage && <p>{submitMessage}</p>}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
-
-
 import React from 'react';
 import '../css/about.css'; // Import the CSS file for the about page styles
 import Image1 from '../images/a.png';
 import Image2 from '../images/Shweta.png';
+import Image3 from '../images/Rupak.png';
+import Image4 from '../images/Gobindo.png';
+import Image5 from '../images/a.png';
+import Image6 from '../images/a.png';
 const About = () => {
   return (
     <div>
@@ -101,7 +48,7 @@ const About = () => {
           </div>
         </div>
         <div className="creator">
-          <img src={Image} alt="Image of the Person" />
+          <img src={Image5} alt="Image of the Person" />
           <div className="creator-info">
             <p>Sayandip Dutta</p>
             <p>Software Developer</p>
@@ -109,7 +56,7 @@ const About = () => {
           </div>
         </div>
         <div className="creator">
-          <img src={Image} alt="Image of the Person" />
+          <img src={Image4} alt="Image of the Person" />
           <div className="creator-info">
             <p>Gobindo </p>
             <p>Software Developer</p>
@@ -117,7 +64,7 @@ const About = () => {
           </div>
         </div>
         <div className="creator">
-          <img src={Image} alt="Image of the Person" />
+          <img src={Image6} alt="Image of the Person" />
           <div className="creator-info">
             <p>Joydeep Das</p>
             <p>Software Developer</p>
@@ -149,7 +96,7 @@ const About = () => {
           </div>
         </div>
         <div className="creator">
-          <img src={Image} alt="Image of the Person" />
+          <img src={Image3} alt="Image of the Person" />
           <div className="creator-info">
             <p>Rupak Choudhury</p>
             <p>Software Developer</p>
