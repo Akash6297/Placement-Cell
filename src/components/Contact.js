d373bc7a04d9b4b190e29d8590e7f13950c20cf0
Akash Mandal <akashmandal6297@gmail.com>
2023-08-24 16:34:27 +0530

    Update

---
+++ src/components/Contact.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import '../css/Contact.css'; // Import your CSS file
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Form successfully submitted
+        alert('Message sent successfully!');
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      } else {
+        // Handle errors, e.g., display an error message to the user
+        alert('Error sending message. Please try again later.');
+      }
+    } catch (error) {
+      // Handle network errors or other exceptions
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="contact-container">
+      
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Message:</label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 
 
-import React, { useState }  from 'react';
-import Axios from 'axios';
+import React   from 'react';
+import Contact from './Contact';
 import '../css/Home.css'; // Import the styles.css file
 import Image4 from '../images/4.jpg'; // Import your image file
 import Image1 from '../images/3.jpg'; // Import your image file
@@ -12,41 +12,20 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carous
 import { Carousel } from 'react-responsive-carousel'; 
 const HomePage = () => {
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    
-    try {
-      // Send a POST request to your backend with the form data
-      const response = await Axios.post('https://placement-p2k8.onrender.com/api/contact', formData);
-      console.log(response.data); // Log the response from the server
-      // Optionally, you can reset the form here
-      setFormData({
-        name: '',
-        email: '',
-        message: '',
-      });
-    } catch (error) {
-      console.error(error);
-      // Handle any errors that occur during the request
-    }
-  };
+    const text = "WELCOME TO RKMGEC PLACEMENT WEBSITE";
+  
+    // Split the text into an array of characters
+    const textArray = text.split('');
   return (
     <div className="container">
-      <h1>WELCOME TO RKMGEC PLACEMENT WEBSITE</h1>
+      <h1>
+        {textArray.map((char, index) => (
+          <span key={index} className="typing-animation">
+            {char}
+          </span>
+        ))}
+      </h1>
       <Carousel autoPlay infiniteLoop showThumbs={false}>
         <div>
           <img src={Image1} alt="Image Description 1" />
@@ -78,36 +57,7 @@ const HomePage = () => {
         
         <center><h2>Contact Us</h2></center>
         <div className="contact-us">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            rows="4"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-          <button type="submit">Submit</button>
-        </form>
+        <Contact />
       </div>
       </div>
       <p>
