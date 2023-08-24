de0444546b51707ec058bf4467d2fba0d89c3979
Akash Mandal <akashmandal6297@gmail.com>
2023-08-24 08:43:00 +0530

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,63 +6,41 @@ import '../css/footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      
       <div className="footer-content">
-      <section>
-        <div className="footer-links">
-          <a href="/"><i className="fas fa-home"></i></a>
-          
-          <a href="/about"><i className="fas fa-info-circle"></i></a>
-          
-          <a href="/process"><i className="fas fa-cogs"></i></a>
-          
-          
-        </div>
-        
-       
-        </section>
         <section>
-        <div className="footer-contact">
-          <p>Contact us:<br/> contact@example.com</p>
-          {/* Add more contact information as needed */}
-        <br/>
-        
-       
-        
-        
-        {/* </section>
-        <section> */}
-        <div className="footer-social">
-          <a href="https://www.facebook.com/yourfacebookpage" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
-        
+          <div className="footer-links">
+            <a href="/" className="footer-link">Home </a>
 
-        {/* </div>
-        <div> */}
-       
-          <a href="https://twitter.com/yourtwitterpage" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          
-          {/* </div>
-          <div> */}
-         
-          <a href="https://www.instagram.com/yourinstagrampage" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-         
-          
-          {/* Add more social media icons and links as needed */}
-        </div>
-        </div>
+            <a href="/about" className="footer-link"> About</a>
+            
+          </div>
+        </section>
+
+        <section>
+          <div className="footer-contact">
+            <p>Contact us: <br /> contact@example.com</p>
+          </div>
+        </section>
+
+        <section>
+          <div className="footer-social">
+            <a href="https://www.facebook.com/yourfacebookpage" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="https://twitter.com/yourtwitterpage" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a href="https://www.instagram.com/yourinstagrampage" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
         </section>
       </div>
+
       <div className="footer-bottom">
         <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-        <a href="https://akash6297.github.io/Akash-Mandal"><p>Created By -Akash Mandal</p></a>
+        <p>Created By <a href="https://akash6297.github.io/Akash-Mandal" className="footer-link">Akash Mandal</a></p>
       </div>
-
     </footer>
   );
 };
