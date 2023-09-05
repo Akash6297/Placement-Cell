ee06c3abf1905d87481dd91c97f3db8d31df51c7
Akash Mandal <akashmandal6297@gmail.com>
2023-09-05 10:53:09 +0530

    Update

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,29 +1,34 @@
 // AdminPanel.js
 
 import React from 'react';
-import '../css/adminpanel.css'
+import { Link } from 'react-router-dom'; // Import the Link component
+import '../css/adminpanel.css';
+
 function AdminPanel() {
   return (
     <div>
-    <h1>WELCOME ADMIN</h1>
-    <hr/>
-    <div className="admin-panel">
-       
-      <div className="admin-box">
-        <h3>Approve Student</h3>
-        {/* Add approval functionality here */}
+      <h1>WELCOME ADMIN</h1>
+      <hr />
+      <div className="admin-panel">
+        <div className="admin-box">
+          <Link to="/approvestudent"> {/* Link to the Internship route */}
+            <h3>Approve Student</h3>
+          </Link>
+          {/* Add approval functionality here */}
+        </div>
+        <div className="admin-box">
+          <Link to="/internship"> {/* Link to the Approve Student route */}
+            <h3>Add Internship</h3>
+          </Link>
+          {/* Add internship addition functionality here */}
+        </div>
+        <div className="admin-box">
+          <Link to="/postnotice"> {/* Link to the Post Notice route */}
+            <h3>Add Notice</h3>
+          </Link>
+          {/* Add notice addition functionality here */}
+        </div>
       </div>
-      <div className="admin-box">
-        <h3>Add Internship</h3>
-        {/* Add internship addition functionality here */}
-      </div>
-      <div className="admin-box">
-        <h3>Add Notice</h3>
-
-        
-        {/* Add notice addition functionality here */}
-      </div>
-    </div>
     </div>
   );
 }
